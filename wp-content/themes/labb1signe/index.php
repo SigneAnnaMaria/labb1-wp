a3f63c54bc82f6e8aba26d1c1f53590ad66daabc
SigneAnnaMaria <signe.annamaria@gmail.com>
2023-04-18 14:07:31 +0200

    Changed the name kontakt.php to page-kontakt.php to get the right content

--- wp-content/themes/labb1signe/index.php
+++ wp-content/themes/labb1signe/index.php
@@ -21,15 +21,6 @@ get_header();
 					}
 					?>
 
-					<!-- <nav class="navigation pagination">
-					<h2 class="screen-reader-text">Inläggsnavigering</h2>
-					<a class="prev page-numbers" href="">Föregående</a>
-					<span class="page-numbers current">1</span>
-					<a class="page-numbers" href="">2</a>
-					<a class="next page-numbers" href="">Nästa</a>
-				</nav> -->
-
-
 					<nav class="navigation pagination">
 						<?php echo paginate_links(); ?>
 						<?php the_posts_pagination(array(
--- wp-content/themes/labb1signe/page-kontakt.php
+++ wp-content/themes/labb1signe/page-kontakt.php
@@ -7,6 +7,7 @@ get_header();
         <div class="container">
             <div class="row">
                 <div class="col-xs-12 col-md-8 col-md-offset-2">
+                    <h1><?php the_title() ?></h1>
                     <?php the_content();
                     ?>
                 </div>
